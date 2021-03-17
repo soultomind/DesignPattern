65729a58a92b6780d5598ba8b36f5308e6db9123
soultomind <soultomind@naver.com>
2021-03-17 20:41:50 +0900

    단순 - Builder 패턴 프로젝트 추가   (1번째 방식, 2번째 방식은 추 후에)

---
+++ Builder/Document.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Document
+    {
+        public Document()
+            : this(String.Empty, String.Empty, String.Empty)
+        {
+
+        }
+        public Document(string title, string author, string text)
+        {
+            Title = title;
+            Author = author;
+            Text = text;
+        }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append("Title=").AppendLine(Title)
+                .Append("Author=").AppendLine(Author)
+                .Append("Text=").AppendLine(Text)
+                .ToString();
+        }
+
+        public class DocumentBuilder
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Text { get; set; }
+
+            public DocumentBuilder SetTitle(string title)
+            {
+                Title = title;
+                return this;
+            }
+
+            public DocumentBuilder SetAuthor(string author)
+            {
+                Author = author;
+                return this;
+            }
+
+            public DocumentBuilder SetText(string text)
+            {
+                Text = text;
+                return this;
+            }
+
+            public Document Build()
+            {
+                return new Document(this);
+            }
+        }
+
+        private Document(DocumentBuilder builder)
+        {
+            Title = builder.Title;
+            Author = builder.Author;
+            Text = builder.Text;
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DesignPattern Builder Pattern");
+
+            Document document = new Document.DocumentBuilder()
+                                .SetTitle("휴가계획서")
+                                .SetAuthor("이바우")
+                                .SetText(DateTime.Now.ToString("yyyy.MM.dd") + " 개인사유로 연차를 사용합니다.")
+                                .Build();
+
+            Console.WriteLine(document);
+        }
+    }
+}
