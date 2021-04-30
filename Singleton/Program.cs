90d9e378e07d1279b4c4e10b3acfa768ab1290c1
soultomind <soultomind@clipsoft.co.kr>
2021-04-30 15:56:33 +0900

    단순 - Singleton 프로젝트 추가

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DesignPattern Singleton Pattern");
+        }
+    }
+}
