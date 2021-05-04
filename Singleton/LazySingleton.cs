34f6e41674248325ecc788318abbbc6e853e1528
soultomind <soultomind@clipsoft.co.kr>
2021-05-04 14:13:18 +0900

    단순 - Singleton 패턴 작업 중

---
+++ Singleton/LazySingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+    class LazySingleton
+    {
+        LazySingleton()
+        {
+            Console.WriteLine("LazySingleton()");
+        }
+        private class Holder
+        {
+            public readonly static LazySingleton Instance = new LazySingleton();
+
+            static Holder()
+            {
+                Console.WriteLine("static Holder()");
+            }
+        }
+
+        public static LazySingleton GetInstance()
+        {
+            return Holder.Instance;
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,6 +10,14 @@ namespace Singleton
         static void Main(string[] args)
         {
             Console.WriteLine("DesignPattern Singleton Pattern");
+
+            LazySingleton o1 = LazySingleton.GetInstance();
+            LazySingleton o2 = LazySingleton.GetInstance();
+
+            if (object.ReferenceEquals(o1, o2))
+            {
+                Console.WriteLine("LazySingleton object.ReferenceEquals o1 == o2 True!");
+            }
         }
     }
 }
